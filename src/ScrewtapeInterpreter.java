7e45da3cc88e2f7259b1de6b009cccbd88b78082
Kabul Totakhil <totakhilkamran7@gmail.com>
2025-04-24 04:56:11 -0700

    Finished execute function

--- src/ScrewtapeInterpreter.java
+++ src/ScrewtapeInterpreter.java
@@ -153,6 +153,51 @@ public class ScrewtapeInterpreter {
   public String execute(String program) {
     // TODO: Implement this
     // If you get stuck, you can look at hint.md for a hint
-    return null;
+
+
+    Map<Integer, Integer> brackets = bracketMap(program);
+    StringBuilder output = new StringBuilder();
+    int i = 0; // program counter
+  
+    while (i < program.length()) {
+      char c = program.charAt(i);
+  
+      if (c == '>') {
+        if (tapePointer.next == null) {
+          tapePointer.next = new Node(0);
+          tapePointer.next.prev = tapePointer;
+        }
+        tapePointer = tapePointer.next;
+      }
+  
+      else if (c == '<') {
+        if (tapePointer.prev == null) {
+          tapePointer.prev = new Node(0);
+          tapePointer.prev.next = tapePointer;
+        }
+        tapePointer = tapePointer.prev;
+      }
+  
+      else if (c == '+') {
+        tapePointer.value++;
+      }
+  
+      else if (c == '-') {
+        tapePointer.value--;
+      }
+  
+      else if (c == '.') {
+        output.append((char) tapePointer.value);
+      }
+  
+      else if (c == ']') {
+        if (tapePointer.value != 0) {
+          i = brackets.get(i);
+        }
+      }
+  
+      i++;
+    }
+    return output.toString();
   }
 }
