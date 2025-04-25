226c8b34087612930805438eda2790120bd21559
Kabul Totakhil <totakhilkamran7@gmail.com>
2025-04-25 16:00:40 -0700

    Add simple and multiple bracketMap tests

--- src/ScrewtapeInterpreterTest.java
+++ src/ScrewtapeInterpreterTest.java
@@ -26,6 +26,39 @@ class ScrewtapeInterpreterTest {
   // TODO: Implement more tests for bracketMap
   // At a bare minimum, implement the other examples from the Javadoc and at least one more you come up with
 
+  @Test
+void testSimpleBracketMap() {
+  // Arrange
+  ScrewtapeInterpreter interpreter = new ScrewtapeInterpreter();
+  String program = "[>+<-]";
+
+  Map<Integer, Integer> expectedMap = new HashMap<>();
+  expectedMap.put(5, 0);
+
+  Map<Integer, Integer> actualMap = interpreter.bracketMap(program);
+
+  // Assert
+  assertEquals(expectedMap, actualMap);
+}
+
+@Test
+void testMultipleBracketPairs() {
+  // Arrange
+  ScrewtapeInterpreter interpreter = new ScrewtapeInterpreter();
+  String program = "[+++][---]<<[+]";
+
+  Map<Integer, Integer> expectedMap = new HashMap<>();
+  expectedMap.put(4, 0);
+  expectedMap.put(9, 5);
+  expectedMap.put(14, 12);
+
+  Map<Integer, Integer> actualMap = interpreter.bracketMap(program);
+
+  // Assert
+  assertEquals(expectedMap, actualMap);
+}
+
+
   
 
   @Test
