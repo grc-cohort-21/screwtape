f8892045f64a8b1c676f872ef1d68b385ae73d46
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-04-30 17:51:30 -0700

    Added testBracketMapWithOnlyBrackets

--- src/ScrewtapeInterpreterTest.java
+++ src/ScrewtapeInterpreterTest.java
@@ -25,6 +25,23 @@ class ScrewtapeInterpreterTest {
 
   // TODO: Implement more tests for bracketMap
   // At a bare minimum, implement the other examples from the Javadoc and at least one more you come up with
+  @Test
+  void testBracketMapWithOnlyBrackets()
+  {
+    // Arrange
+    ScrewtapeInterpreter interpreter = new ScrewtapeInterpreter();
+    String program = "[]";
+
+    Map<Integer, Integer> expectedMap = new HashMap<>();
+    expectedMap.put(1, 0);
+
+    // Act
+    Map<Integer, Integer> actualMap = interpreter.bracketMap(program);
+
+    // Assert
+    assertEquals(expectedMap, actualMap);
+
+  }
 
   
 
